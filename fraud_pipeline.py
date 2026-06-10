f7c7042137009ee987e8c20ba67b7186371456f8
Dimitar9898 <dimitar942@gmail.com>
2026-06-09 23:26:40 -0400

    initla fraud detection pipeline

---
+++ fraud_pipeline.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, split, avg, count
+
+# Creating a spark session
+spark = (SparkSession.builder
+    .appName("Fraud Detection")
+    .getOrCreate()
+)
+
+# so first import the main psyarpk tool, spark session is entry point, then import specific functions we need to use later
+# spark session builder makes the session so connection to the spark engine, the app name gives the job a name
+# get or creta makes a new session or reuses one
+# the \ is a lien continuation character, so we can write the code on multiple lines for readability
+
+# reading the csv file into a spark dataframe, we specify the path to the file, and some options
+df = spark.read.csv('fraudTest.csv', header=True, inferSchema=True)
+
+#Split date and time into separate column
+df = df.withColumn("trans_date", split(col('trans_date_trans_time'), " ")[0])
+df = df.withColumn("trans_time", split(col('trans_date_trans_time'), " ")[1])
+
+# spark read csv it reads the csv file into a spark data frame, header true means first row is column names, infer scvhema is 
+# so datatypes are figured out, df with column adds a new column to the dataframe, takes name of new column and what to put in
+# split col, spilts that combined column with two parts, the split is on the  space in the actual values
+
+df.show(5)
+
+# calculate fraud rate by category
+df = df.drop("_c0")
+
+fraud_by_category = (df.groupBy("category")
+    .agg(
+        count("is_fraud").alias("total_transactions"),
+        avg("is_fraud").alias("fraud_rate")
+    )
+    .withColumn("fraud_rate", (col("fraud_rate") * 100).cast("decimal(5,2)"))
+    .orderBy(col("fraud_rate").desc())
+)
+
+fraud_by_category.show()
